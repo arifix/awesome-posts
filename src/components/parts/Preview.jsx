f2d75f3a79c3e708a297e22ed6932cbd76cc6485
Arif Khan <arifkpi@gmail.com>
2024-06-22 23:25:30 +0600

    Preview minor update

--- src/components/parts/Preview.jsx
+++ src/components/parts/Preview.jsx
@@ -133,36 +133,39 @@ const Preview = ({ defaultSettings, cssStyles }) => {
                     alt={demoPost.title}
                     className="ap-featured-img"
                   />
-                  <div className="ap-date">20 March</div>
+                  {defaultSettings.postMetaDisDate && (
+                    <div className="ap-date">20 March</div>
+                  )}
                 </div>
               )}
               <div className="ap-post-meta">
-                <a href="#">
-                  <svg
-                    fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    width="24"
-                    height="24"
-                  >
-                    <path
-                      d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"
-                    />
-                  </svg>
-                  <span className="ap-meta">Admin</span>
-                </a>
-
-                <a href="#">
-                  <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                    ></path>
-                  </svg>
-                  <span className="ap-meta">10 Comments</span>
-                </a>
+                {defaultSettings.postMetaDisAuthor && (
+                  <a href="#">
+                    <svg
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      width="24"
+                      height="24"
+                    >
+                      <path d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z" />
+                    </svg>
+                    <span className="ap-meta">Admin</span>
+                  </a>
+                )}
+                {defaultSettings.postMetaDisCC && (
+                  <a href="#">
+                    <svg fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                      ></path>
+                    </svg>
+                    <span className="ap-meta">10 Comments</span>
+                  </a>
+                )}
               </div>
               <div className="ap-post-content">
                 {defaultSettings.displayPostCategory && (
